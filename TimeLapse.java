387cfc4394ee62dade1bf9dedcfc92d0b6bc74d2
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-24 22:09:37 -0300

    ...

--- TimeLapse.java
+++ TimeLapse.java
@@ -14,15 +14,41 @@ public class TimeLapse{
 
 		File imageFile = new File(name+".png");
 		ImageIO.write(capture, "png", imageFile );
+		System.out.println(name+".png");
 	}
 
-	public static void main(String[]args) throws Exception{
-		int i = 0;
+	public static void run(String[]args) throws Exception{
+		int i = getLast(args[0])+1;
+
 		DecimalFormat df = new DecimalFormat("#000");
 		while(true){
 			capture(args[0]+"/"+df.format(i++)+"");
 			Thread.sleep(33*Integer.parseInt(args[1]));
 		}
+	}
+
+	public static int getLast(String folder) throws Exception{
+		System.out.println(folder);
+		File dir = new File(folder);
+		File[] ls = dir.listFiles();
+			
+		int higher = 0;
+		
+		try{
+			for(File f : ls){
+				String name = f.getName().split("\\.")[0];
+				
+				higher = (Integer.parseInt(name) > higher)?Integer.parseInt(name):higher;
+			}
+		}catch(Exception e){}
+
+		System.out.println("last:"+higher);
+		
+		return higher;
+	}
+
+	public static void main(String[]args) throws Exception{
+		run(args);
 	}
 
 }
---
+++ bin/time-lapse/TimeLapseWithJava.bat
@@ -0,0 +1,4 @@
+@echo off
+mkdir %temp%\time-lapse
+mkdir %temp%\time-lapse\captured
+java TimeLapse %temp%\time-lapse\captured 40
